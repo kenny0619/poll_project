60874271fa58897872d811de26e504472f14e0ac
kenny0619 <kehindeibukun0619@gmail.com>
2020-12-11 17:40:03 +0100

    created vote view

--- poll/views.py
+++ poll/views.py
@@ -32,5 +32,25 @@ def results(request, poll_id):
     return render(request, 'poll/results.html', context)
 
 def vote(request, poll_id):
-    context = {}
+    poll = Poll.objects.get(pk=poll_id)
+
+    if request.method == 'POST':
+
+        selected_option = request.POST['poll']
+        if selected_option == 'option1':
+            poll.option_one_count += 1
+        elif selected_option == 'option2':
+            poll.option_two_count += 1
+        elif selected_option == 'option3':
+            poll.option_three_count += 1
+        else:
+            return HttpResponse(400, 'Invalid form option')
+
+        poll.save()
+
+        return redirect('results', poll.id)
+
+    context = {
+        'poll' : poll
+    }
     return render(request, 'poll/vote.html', context)
